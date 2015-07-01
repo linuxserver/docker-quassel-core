791698d7af41ba3b679ba07df05db26cf80f3ca1
Stian Larsen <lonixx@gmail.com>
2015-07-01 12:51:33 +0200

    Added a webui runit daemon to installer

--- init/20_install_web.sh
+++ init/20_install_web.sh
@@ -11,3 +11,13 @@ if [ "$WEBUI" == "1" ]; then
 	if [ ! -f /config/quassel-webserver/settings-user.js ]; then
 		cp -v /default/settings-user.js /config/quassel-webserver/settings-user.js
 	fi
+	if [ ! -f /etc/service/webui/run ]; then
+		cat <<'EOT' > /etc/service/webui/run
+			#!/bin/bash
+
+			exec /sbin/setuser abc node /config/quassel-webserver/app.js -m http -p 64443
+
+
+			EOT	
+else
+	rm -fr /etc/service/webui 
